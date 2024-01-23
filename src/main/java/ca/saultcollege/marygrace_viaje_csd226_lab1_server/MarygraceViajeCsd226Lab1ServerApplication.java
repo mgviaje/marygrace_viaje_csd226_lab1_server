2e650c982e205d4f9de9798f881ec7e0da801416
mgviaje <113563698+mgviaje@users.noreply.github.com>
2024-01-22 16:15:03 -0800

    Initial commit

---
+++ src/main/java/ca/saultcollege/marygrace_viaje_csd226_lab1_server/MarygraceViajeCsd226Lab1ServerApplication.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.marygrace_viaje_csd226_lab1_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarygraceViajeCsd226Lab1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarygraceViajeCsd226Lab1ServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/saultcollege/marygrace_viaje_csd226_lab1_server/MarygraceViajeCsd226Lab1ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.saultcollege.marygrace_viaje_csd226_lab1_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarygraceViajeCsd226Lab1ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
